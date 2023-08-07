28c183660576a7c57bf514cc2167a587ae5bf9f4
Miguel Garoz <mgaroz@gmail.com>
2023-08-07 04:19:32 -0600

    feat(app): added embla carousel

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import emblaCarouselSvelte from 'embla-carousel-svelte';
+	import Autoplay from 'embla-carousel-autoplay';
+	import img1 from '$lib/img/car0.webp';
+	import img2 from '$lib/img/car1.webp';
+	import img3 from '$lib/img/car2.webp';
+
+	const items = [
+		{ imageUrl: img1, caption: 'Image 1' },
+		{ imageUrl: img2, caption: 'Image 2' },
+		{ imageUrl: img3, caption: 'Image 3' }
+	];
+
+	let emblaApi;
+	const config = {
+		options: {
+			slidesToScroll: 1,
+			loop: true
+		}
+	};
+
+	const options = {
+		slidesToSroll: 1,
+		loop: true
+	};
+	let plugins = [Autoplay()];
+
+	const onInit = (event: any) => {
+		emblaApi = event.detail;
+	};
+</script>
+
+<section class="relative -z-0">
+	<div class="embla" use:emblaCarouselSvelte={{ options, plugins }} on:init={onInit}>
+		<div class="embla__container">
+			{#each items as { imageUrl, caption }}
+				<div class="embla__slide">
+					<img loading="lazy" src={imageUrl} alt={caption} width="100%" height="100%" />
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
+
+<style>
+	.embla {
+		overflow: hidden;
+	}
+
+	.embla__container {
+		display: flex;
+	}
+
+	.embla__slide {
+		overflow: hidden;
+		flex: 1;
+		min-width: 100vw;
+		position: relative;
+		height: 100vh;
+	}
+
+	.embla__slide img {
+		transition: transform 0.5s cubic-bezier(0.165, 0.84, 0.44, 1);
+		object-fit: cover;
+		height: 100%;
+		width: 100%;
+	}
+</style>
