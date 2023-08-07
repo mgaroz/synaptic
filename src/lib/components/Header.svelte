398341ed6f4cfeba55c801782dce84fad7ccbe4a
Miguel Garoz <mgaroz@gmail.com>
2023-08-07 02:47:13 -0600

    first commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,7 @@
+<div
+	class="flex justify-between items-center px-8 h-[3.875rem] w-[1160px] rounded-[0.9375rem] backdrop-blur-[10px] bg-[#1b1a25]/50 shadow-md text-white"
+>
+	<div>Menu Left</div>
+	<div>Menu Center</div>
+	<div>Menu Right</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<header class="absolute left-0 right-0 top-6 mx-auto w-[1160px]">
+	<Header />
+</header>
+<main
+	class="min-h-[100vh] bg-[#1A1924] text-white"
+	style="background-image: url('https://picsum.photos/id/35/1920/1080')"
+>
+	<div id="body" class="pt-28">
+		<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</div>
+</main>
