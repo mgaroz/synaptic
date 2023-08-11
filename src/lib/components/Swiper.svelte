2c4f2d09f602670691e12cd9f1040463d92110ab
Miguel Garoz <mgaroz@gmail.com>
2023-08-11 02:21:24 -0600

    feat(app): navigation buttons

--- src/lib/components/Swiper.svelte
+++ src/lib/components/Swiper.svelte
@@ -40,14 +40,40 @@
 					data-swiper-parallax-duration={700}
 				>
 					<div class="flex w-full justify-between">
-						<div class="delay flex flex-col gap-2">
+						<div class="delay flex flex-col gap-6">
 							<h1 class="text-8xl text-white">Title</h1>
 							<p class="text-3xl">Subtitle</p>
 							<a href="/" class="text-xl">Link</a>
 						</div>
-						<div>
-							<div class="h-[50px] w-[50px] rounded-full border border-solid" />
-							<h2>here</h2>
+						<div class="flex flex-col gap-4">
+							<div
+								class="flex h-[50px] w-[50px] items-center justify-center rounded-full border border-solid"
+							>
+								<svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+									<path
+										fill="none"
+										stroke="#ffffff"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="1"
+										d="M4 12h16m0 0l-6-6m6 6l-6 6"
+									/>
+								</svg>
+							</div>
+							<div
+								class="flex h-[50px] w-[50px] items-center justify-center rounded-full border border-solid"
+							>
+								<svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+									<path
+										fill="none"
+										stroke="#ffffff"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="1"
+										d="M20 12H4m0 0l6-6m-6 6l6 6"
+									/>
+								</svg>
+							</div>
 						</div>
 					</div>
 				</div>
