1f142ce8c1bc674ff7908fde97dc624c51f726e4
Miguel Garoz <mgaroz@gmail.com>
2023-08-17 23:30:51 -0600

    feat(app): stacked divs

--- src/lib/components/Entry.svelte
+++ src/lib/components/Entry.svelte
@@ -2,21 +2,18 @@
 	export let image: any;
 </script>
 
-<div class="relative h-full">
-	<img
-		loading="lazy"
-		src={image}
-		alt="something"
-		class="absolute left-0 right-0 z-0 object-cover"
-	/>
-	<div
-		class="absolute bottom-0 left-0 z-10 flex h-full flex-col justify-end opacity-0 hover:opacity-100"
-	>
-		<p>Text one</p>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro optio dolorum facere
-			necessitatibus, quibusdam dolorem dicta autem aliquid quo natus ab maxime sequi quia
-			repudiandae doloremque tenetur sapiente exercitationem consequuntur.
-		</p>
+<div class="relative h-full w-full">
+	<div class="group absolute inset-0 flex h-full flex-col">
+		<img loading="lazy" src={image} alt="something" class="absolute inset-0 z-0 object-cover" />
+		<div
+			class="absolute inset-0 z-10 flex h-full flex-col justify-end opacity-0 group-hover:opacity-100"
+		>
+			<p>Text one</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro optio dolorum facere
+				necessitatibus, quibusdam dolorem dicta autem aliquid quo natus ab maxime sequi quia
+				repudiandae doloremque tenetur sapiente exercitationem consequuntur.
+			</p>
+		</div>
 	</div>
 </div>
