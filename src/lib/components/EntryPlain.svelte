38918dc8b2d02328745b6835bc0260c87451c7e4
Miguel Garoz <mgaroz@gmail.com>
2023-08-26 00:02:11 -0600

    feat(app): changed subtitle font size

--- src/lib/components/EntryPlain.svelte
+++ src/lib/components/EntryPlain.svelte
@@ -1,7 +1,7 @@
 <div class="flex h-full flex-col items-center justify-center">
 	<div>
 		<h2 class="text-4xl font-bold">Title</h2>
-		<p>Subtitle</p>
+		<p class="text-2xl">Subtitle</p>
 		<p>Link</p>
 	</div>
 </div>
