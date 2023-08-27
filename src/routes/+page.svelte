47e0f6bdbbc83b37066915d0f130fd82d504dd9e
Miguel Garoz <mgaroz@gmail.com>
2023-08-27 00:19:10 -0600

    feat(app): grid using custom %

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,16 +23,10 @@
 			<Entry image={imagethree} />
 			<EntryPlain />
 		</div>
-		<div class="grid grid-cols-7 gap-2 pt-2">
-			<div class="col-span-2">
-				<Entry image={imageone} />
-			</div>
-			<div class="col-span-2">
-				<Entry image={imagetwo} />
-			</div>
-			<div class="col-span-3">
-				<Entry image={imagethree} />
-			</div>
+		<div class="grid grid-cols-[28%_28%_44%] gap-2 pt-2">
+			<Entry image={imageone} />
+			<Entry image={imagetwo} />
+			<Entry image={imagethree} />
 		</div>
 		<!-- <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
